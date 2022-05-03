d650ccd3c29c77b56ec20da863c54c1084714acc
Pratishtha-searce <89970393+Pratishtha-searce@users.noreply.github.com>
2022-05-03 23:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)   #app instance
 #root of application 
 @app.route("/")       #app as a decorator to create each router or URL provided by application 
 def hello(): 
-    return "Hello hii, I m Pratishtha agarwal, Welcome to Searce, nice to meet you all :)" 
+    return "Hello hii, I m Pratishtha agarwal :)" 
 
  
 if __name__ == "__main__": 
